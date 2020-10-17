494fe991f0b82c1b605883a2d46a2fe310cd8ac6
FaizanNehal <faizannehal99@gmail.com>
2020-10-18 03:03:44 +0500

    added python file

--- ReconNow.sh
+++ ReconNow.sh
@@ -6,4 +6,14 @@ echo -e "_______     _______      _______     ______      ___   __         ___  
         |  _  __/    |  ____|    | |         | |  | |    |  |\    |       |  |\    |    | |  | |    |  | |  | |  |
         | | \ \      | |____     | |_____    | |__| |    |  | \   |       |  | \   |    | |__| |    |  |_|  |_|  |
         |_|  \_\     |______|    \_______|   \______/    |__|  \__|       |__|  \__|    \______/    |____________|
+                                                                                                        v-0.01 Faizan Nehal
 "
+hrlp(){
+echo -e "
+        -u Specify the domain you want to do recon
+        -p Tell the ports you want to scan (default 80,443). If you want to scan all ports then use --all
+
+
+        --omit-ss If you want to omit Screenshotting then use this flag
+"
+}
